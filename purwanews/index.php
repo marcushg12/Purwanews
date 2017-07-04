4ae5a9185dfc796b855a6e476d96da11ae53900f
prmdbina <prmdbnpalupi@gmail.com>
2017-07-04 16:59:57 +0700

    Add files via upload

---
+++ purwanews/index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>PurwaNews</title>
+
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/index.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<!--header-->
+
+	<div class="content content-header">
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#">
+		      	<img src="image/PN1.jpg" height="30" width="75">
+		      </a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <form class="navbar-form navbar-right">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default">Submit</button>
+		        <a href="#">Sign up / Login</a>
+		      </form>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+
+	<div class="content content-header">
+		<nav class="navbar navbar-default navbar-footer">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		     <!-- <a class="navbar-brand" href="#">
+		      	<img src="image/PN1.jpg" height="30" width="75">
+		      </a> -->
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      <ul class="nav nav-tabs nav-justified">
+		        <li><a href="#">News</a></li>
+		        <li><a href="#">Ekonomi</a></li>
+		        <li><a href="#">Internet</a></li>
+		        <li><a href="#">Olahraga</a></li>
+		        <li><a href="#">Otomotif</a></li>
+		        <li><a href="#">Travel</a></li>
+		        <li><a href="#">Kuliner</a></li>
+		        <li><a href="#">Kesehatan</a></li>
+		        <li><a href="#">Entertainment</a></li>
+		        <li><a href="#">Edukasi</a></li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</div>
+
+
+<!--content-->
+
+
+	<div class="content content-body">
+		<div class="container-fluid">
+			<div class="row" align="center">
+			  <div class="col-md-2">
+			  	Instagram menjadi salah satu media sosial yang digemari oleh banyak orang di dunia. Segala momen diabadikan dan di-upload ke Instagram demi mendapatkan banyak likes hingga followers. Lewat Instagram, kalian bisa memperlihatkan kehidupan sehari-hari yang dinilai menarik.
+
+				Bahkan banyak orang yang melakukan segala aktivitas demi di-share di Instagram. Tak heran jika Instagram sudah menjadi gaya hidup masyarakat masa kini. Ada hal yang harus diperhatikan untuk membuat feed Instagram menjadi lebih menarik, yakni aplikasi meng-edit foto. Berikut deretan aplikasi yang bisa dicoba:
+			  </div>
+			  <div class="col-md-8">
+			    Instagram menjadi salah satu media sosial yang digemari oleh banyak orang di dunia. Segala momen diabadikan dan di-upload ke Instagram demi mendapatkan banyak likes hingga followers. Lewat Instagram, kalian bisa memperlihatkan kehidupan sehari-hari yang dinilai menarik.
+
+				Bahkan banyak orang yang melakukan segala aktivitas demi di-share di Instagram. Tak heran jika Instagram sudah menjadi gaya hidup masyarakat masa kini. Ada hal yang harus diperhatikan untuk membuat feed Instagram menjadi lebih menarik, yakni aplikasi meng-edit foto. Berikut deretan aplikasi yang bisa dicoba:
+
+			  </div>
+			  <div class="col-md-2">
+			    Instagram menjadi salah satu media sosial yang digemari oleh banyak orang di dunia. Segala momen diabadikan dan di-upload ke Instagram demi mendapatkan banyak likes hingga followers. Lewat Instagram, kalian bisa memperlihatkan kehidupan sehari-hari yang dinilai menarik.
+
+				Bahkan banyak orang yang melakukan segala aktivitas demi di-share di Instagram. Tak heran jika Instagram sudah menjadi gaya hidup masyarakat masa kini. Ada hal yang harus diperhatikan untuk membuat feed Instagram menjadi lebih menarik, yakni aplikasi meng-edit foto. Berikut deretan aplikasi yang bisa dicoba:
+
+			  </div>
+			</div>
+		</div>
+	</div>
+
+<!--footer-->
+
+	<div class="content content-footer">
+		<footer>
+		    <div class="footer" id="footer">
+		        <div class="container">
+		            <div class="row">
+		                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+		                	<h3> News </h3>
+		                    <ul>
+		                        <li> <a href="#"> Nasional</a> </li>
+		                        <li> <a href="#"> Regional </a> </li>
+		                        <li> <a href="#"> Megapolitan </a> </li>
+		                        <li> <a href="#"> Internasional </a> </li>
+		                    </ul>
+		                </div>
+		                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+		                	<h3> Sains </h3>
+		                    <ul>
+		                        <li> <a href="#"> Edukasi </a> </li>
+		                        <li> <a href="#"> Olahraga</a> </li>
+		                        <li> <a href="#"> Ekonomi </a> </li>
+		                        <li> <a href="#"> Internet </a> </li>
+		                    </ul>
+		                </div>
+		                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+		                	<h3> Entertainment </h3>
+		                    <ul>
+		                        <li> <a href="#"> Otomotif </a> </li>
+		                        <li> <a href="#"> Travel</a> </li>
+		                        <li> <a href="#"> Kesehatan </a> </li>
+		                        <li> <a href="#"> Lifestyle </a> </li>
+		                    </ul>
+		                </div>
+		                <div class="col-lg-2  col-md-2 col-sm-4 col-xs-6">
+		                	<h3> Indeks Berita </h3>
+		                    <ul>
+		                        <li> <a href="#"> Headline </a> </li>
+		                        <li> <a href="#"> Topik Pilihan </a> </li>
+		                        <li> <a href="#"> Terpopuler </a> </li>
+		                    </ul>
+		                </div>
+		                <div class="col-lg-3  col-md-3 col-sm-6 col-xs-12 ">
+		                    <h3> PurwaNews </h3>
+		                    <ul>
+		                        <li>
+		                            <div class="input-append newsletter-box text-center">
+		                                <input type="text" class="full text-center" placeholder="Email ">
+		                                <button class="btn  bg-gray" type="button"> Kirim <i class="fa fa-long-arrow-right"> </i> </button>
+		                            </div>
+		                        </li>
+		                    </ul>
+		                    <ul class="social">
+		                        <li> <a href="#"> <i class="fa fa-facebook-square"></i> </a> </li>
+		                        <li> <a href="#"> <i class="fa fa-twitter"></i> </a> </li>
+		                        <li> <a href="#"> <i class="fa fa-google-plus"></i> </a> </li>
+		                        <li> <a href="#"> <i class="fa fa-pinterest-square"></i> </a> </li>
+		                        <li> <a href="#"> <i class="fa fa-youtube"></i> </a> </li>
+		                    </ul>
+		                </div>
+		            </div>
+		            <!--/.row--> 
+		        </div>
+		        <!--/.container--> 
+		    </div>
+		    <!--/.footer-->
+		    
+		    <div class="footer-bottom">
+		        <div class="container">
+		            <p class="" align="center"> Copyright © PurwaNews 2017. </p>
+		        </div>
+		    </div>
+		    <!--/.footer-bottom--> 
+		</footer>
+	</div>
+
+</body>
+</html>
